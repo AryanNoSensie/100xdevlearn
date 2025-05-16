d7997aa47622ceba9a7e3ce33154f5d03fc409bc
AryanNoSensie <officearyan14@gmail.com>
2025-05-16 08:40:00 +0530

    done with jwt reacp now bui;diong frontend

---
+++ week6.2/script.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken'); // Importing the jsonwebtoken library
+const JWT_SECRET = "heytherearyan";
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+//createing a inmomeory varibale 
+const users = [];
+
+//route to signup
+app.post('/signup' , async (req,res) =>{
+ const username = req.body.username;
+ const password = req.body.password;
+
+ users.push({
+    username,
+    password
+ })
+ res.json({
+    message : "user is signed up successfully"
+ })
+console.log(users);
+})
+
+
+
+//route to signin
+app.post('/signin' , async (req,res) =>{
+    const username  = req.body.username;
+    const password  = req.body.password;
+
+    const founduser = users.find(user => user.username == username && user.password == password);
+
+
+    if(founduser){
+        const token = jwt.sign({
+            username : username
+        }, JWT_SECRET);
+
+        res.json({
+            message : "user is signed in successfully and here is the token of his account",
+            token
+        })
+    }
+    else{
+        res.status(401).json({
+            message : "user is not found"
+        })
+    }
+    
+
+})
+
+//auth middleware 
+function auth(req,res,next){
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.username = decoded.username;
+
+}
+
+//me rourte to get the username bqack from token requested 
+
+
+app.get('/me', auth , (req,res) =>{
+   
+    
+    
+    const founduser = users.find(user => user.username === req.username);
+
+    if(founduser){
+        res.json({
+            message : "user is found" ,
+            username : req.username,
+            password : founduser.password
+             
+
+        })
+    }
+    else{
+        res.status(401).json({
+            message : "user is not found"
+        })
+    }
+
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
