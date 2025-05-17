9f389aed7973640471c6d6cbd81253a0d3acf393
AryanNoSensie <officearyan14@gmail.com>
2025-05-17 09:35:53 +0530

    now going to work on auht login page

--- sherAuth1/script.js
+++ sherAuth1/script.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
+// const express = require('express');
+// // const cookieParser = require('cookie-parser');
+// const app = express();
+// app.use(express.json());
+// // app.use(cookieParser());
 
+
+// app.get('/elf', (req, res) => {
+//   res.cookie('username', 'JohnDoe');
+//   res.json({
+//     message: 'Cookie set successfully',
+  
+//   })
+// })
+
+// // app.get('/read/', (req, res) => {
+// //     console.log(req.cookies);
+// //     res.send("read the pagesmf");
+// // });
+
+
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
 
 app.get('/elf', (req, res) => {
-  res.cookie('username', 'JohnDoe');
-  res.json({
-    message: 'Cookie set successfully',
-  
-  })
-})
-
-app.get('/read/', (req, res) => {
-    console.log(req.cookies);
-    res.send("read the pagesmf");
+ bcrypt.compare("aryanisgreat", "$2b$10$2A4KM1aoKIewfw8Q3k3.QOG2QJrbhHLaRjZ0s9bgDldaasmqjIUfG", function(err, result) {
+    // result == truecons
+    console.log(result);
 });
-
-
-
-
+})
 
 
 app.listen(3000, () => {
